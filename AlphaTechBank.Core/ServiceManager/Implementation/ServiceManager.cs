f07ad2011a17568fa84e488c44125a489dbd4dd2
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2023-08-01 22:06:34 +0100

    Service

--- AlphaTechBank.Core/ServiceManager/Implementation/ServiceManager.cs
+++ AlphaTechBank.Core/ServiceManager/Implementation/ServiceManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AlphaTechBank.Core.ServiceManager.Abstraction;
+using AlphaTechBank.Core.Services.Abstraction;
+using AlphaTechBank.Core.Services.Implementation;
+using AlphaTechBank.Repository.Repository.UnitOfWork.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace AlphaTechBank.Core.ServiceManager.Implementation
 {
-    internal class ServiceManager
+    public class ServiceManager : IServiceManager
     {
+        private IAuthenticationService _authenticationService;
+        private IAccountService _accountService;
+        private ITransactionService _transactionService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ServiceManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IAuthenticationService AuthenticationService =>
+            _authenticationService ?? new AuthenticationService(_unitOfWork);
+
+        public IAccountService AccountService => 
+            _accountService ?? new AccountService(_unitOfWork);
+
+        public ITransactionService TransactionService => 
+            _transactionService ?? new TransactionService(_unitOfWork);
     }
 }
